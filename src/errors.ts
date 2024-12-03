3dbba819f3dbd53712f3cd313db523b6f4a54c2e
Nico Flaig <nflaig@protonmail.com>
2024-12-03 22:47:44 +0100

    Update Lodestar dependencies

--- src/errors.ts
+++ src/errors.ts
@@ -1,13 +1,13 @@
-import { HttpStatusCode } from "@lodestar/api";
+import { ApiError, HttpStatusCode } from "@lodestar/api";
 
 export class LodestarError extends Error {
     public code: HttpStatusCode
     public operationId: string | undefined
     public message: string
 
-    constructor(error: { code: HttpStatusCode, message?: string, operationId?: string } ) {
+    constructor(error: ApiError ) {
         super("LodestarError")
-        this.code = error.code
+        this.code = error.status
         this.message = error.message || "unknown error"
         this.operationId = error.operationId
      }
